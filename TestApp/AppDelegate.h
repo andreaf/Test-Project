562ba2a346e2d1ca08abddeb1f6167575360db61
andrea <andrea@mistral.deltatre.it>
2012-02-03 18:59:07 +0100

    Initial Commit

---
+++ TestApp/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  TestApp
+//
+//  Created by andrea fanfani on 03/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ TestApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestApp
+//
+//  Created by andrea fanfani on 03/02/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
